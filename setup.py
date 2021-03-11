326b88609e3f0339e2517317fecb91c52aa38e78
Joseph Salmingo <joseph.salmingo@gmail.com>
2021-03-11 11:34:54 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='corona-taiou',
+    version='0.1',
+    packages=['corona-taiou'],
+    url='https://github.com/joooooooooe-star/corona-taiou-webapp',
+    license='MIT',
+    author='joseph',
+    author_email='joseph.salmingo@gmail.com',
+    description='flask app for tracking healtih information'
+)
