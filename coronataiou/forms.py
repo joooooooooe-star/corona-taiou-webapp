f06af3c4fd99ca51102810203d0703763e413da3
joseph-salmingo <joseph.salmingo@gmail.com>
2021-03-17 13:55:57 +0900

    basic date input form and route

--- coronataiou/forms.py
+++ coronataiou/forms.py
@@ -2,9 +2,12 @@
 
 from flask_wtf import Form
 from wtforms.fields.html5 import DateField
+from wtforms import SubmitField
 
 
 class DatePickerForm(Form):
     """A simple form for picking the date"""
-    start_date = DateField("Start Date", format='%Y-%m-%m')
-    end_date = DateField("End Date", format='%Y-%m-%m')
+    startdate = DateField("Start Date", format='%Y-%m-%m')
+    enddate = DateField("End Date", format='%Y-%m-%m')
+    submit = SubmitField('Search')
+
