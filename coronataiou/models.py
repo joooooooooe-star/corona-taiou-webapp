8e530e4aa1f63a10f463ad02c1886e6e6be6267a
Joseph Salmingo <joseph.salmingo@gmail.com>
2021-03-18 18:22:47 +0900

    models for changed and removed data

--- coronataiou/models.py
+++ coronataiou/models.py
@@ -26,6 +26,38 @@ class RecordData(db.Model):
         self.updated = updated
 
 
+class ChangedRecordData(db.Model):
+    """Contains the records for any data that has been modified."""
+    __tablename__ = 'changed_records'
+    id = db.Column(db.Integer, primary_key=True)
+    record_id = db.Column(db.Integer)
+    oldname = db.Column(db.String)
+    newname = db.Column(db.String)
+    oldtemperature = db.Column(db.Float)
+    newtemperature = db.Column(db.Float)
+    oldfatigue = db.Column(db.Boolean)
+    newfatigue = db.Column(db.Boolean)
+    oldsore_throat = db.Column(db.Boolean)
+    newsore_throat = db.Column(db.Boolean)
+    oldother_pain = db.Column(db.Boolean)
+    newother_pain = db.Column(db.Boolean)
+    dateofchange = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class RemovedRecordData(db.Model):
+    """Contains the records for any data that has been deleted."""
+    __tablename__ = 'deleted_records'
+    id = db.Column(db.Integer, primary_key=True)
+    record_id = db.Column(db.Integer)
+    name = db.Column(db.String)
+    temperature = db.Column(db.Float)
+    fatigue = db.Column(db.Boolean)
+    sore_throat = db.Column(db.Boolean)
+    oldother_pain = db.Column(db.Boolean)
+    lastupdated = db.Column(db.DateTime)
+    delete_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 class RecordSchema(ma.SQLAlchemySchema):
     class Meta:
         # Return everything but id and update time
